4455056c87a4bbcfae3d2a2c00a11fe83eb48f92
Nadeen <75531472+NadeenXo@users.noreply.github.com>
2024-10-31 23:11:51 +0300

    search fragment

--- app/src/main/java/com/example/aroundegypt/SearchFragment.kt
+++ app/src/main/java/com/example/aroundegypt/SearchFragment.kt
@@ -6,12 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class ExperienceFragment : Fragment() {
+class SearchFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_experience, container, false)
+        return inflater.inflate(R.layout.fragment_search, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
